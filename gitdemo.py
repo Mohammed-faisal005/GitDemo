864a39888a2fb7cf3b4d4119f3de1eee57be9349
Faisal <faisal.786005@gmail.comgit add .git push origin main$ git config --global user.email faisal.786005@gmail.com>
2023-01-04 20:18:39 +0530

    SpiderMan

--- gitdemo.py
+++ gitdemo.py
@@ -2,8 +2,5 @@ print("Big data Engineer")
 print("Ineuron")
 print("IronMan")
 print("Hulk")
-<<<<<<< HEAD
 print("SpiderMan")
-=======
 print("PeterParker")
->>>>>>> da803e9a80f2e978529857f0e5d5148c259461ae
